b18f0ad57b4ffc2ea083fbc38fc214a26f3c628f
judithRehanga <rehanga2810w@gmail.com>
2026-07-28 14:19:45 +0530

    Fix upload API URL

--- src/pages/ReviewPage/uploadService.js
+++ src/pages/ReviewPage/uploadService.js
@@ -2,11 +2,7 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-export const uploadFiles = async (
-  files,
-  eventSlug,
-  onUploadProgress
-) => {
+export const uploadFiles = async (files, eventSlug, onUploadProgress) => {
   const formData = new FormData();
 
   files.forEach((item) => {
@@ -15,7 +11,7 @@ export const uploadFiles = async (
 
   formData.append("event", eventSlug);
 
-  return axios.post(`${API}/upload`, formData, {
+  return axios.post(`${API_URL}/upload`, formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
